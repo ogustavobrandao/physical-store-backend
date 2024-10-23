8a64a401cd98afde038fad8b575c80eaa5b61edd
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-10-22 22:54:46 -0300

    Configuração inicial do projeto e da conexão com o banco de dados.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-const express = require('express');
+import express from 'express';
+import sequelize from './config/database';
 
 const app = express();
 
 
 app.listen(3000, () => {
     console.log('Servidor em execução em http://localhost:3000');
+    sequelize.sync();
 })
