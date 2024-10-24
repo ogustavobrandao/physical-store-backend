32546f6aeea5cdf881f86e7824d432c672950d8a
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-10-23 21:48:16 -0300

    Criação de model de loja, configuração do TypeScript para suporte a decorators.

---
+++ src/models/Loja.ts
@@ -0,0 +1,35 @@
+import {Table, Column, Model, DataType} from 'sequelize-typescript'
+
+@Table({
+    tableName: 'lojas',
+    timestamps: true,
+    underscored: true,
+})
+
+export class Loja extends Model {
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    nome!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    logradouro!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    cep!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true,
+    })
+    telefone?: string;
+
+
+}
