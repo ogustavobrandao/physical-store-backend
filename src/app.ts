d30bb6925ce7b82a1573f3250bb7684031ebb2d1
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-10-30 23:18:50 -0300

    Ajustes no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import express from 'express';
 import sequelize from './config/database';
+import lojaRoutes from './routes/lojaRoutes';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
+app.use(express.json()); 
 
-app.listen(3000, () => {
-    console.log('Servidor em execução em http://localhost:3000');
-    sequelize.sync();
-})
+app.use('/api', lojaRoutes);
+
+app.use(errorHandler);
+
+sequelize.sync()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Servidor em execução em http://localhost:3000');
+        })
+    })
+    .catch(error => {
+        console.error('Erro ao conectar ao banco de dados:', error);
+    });
+
+export default app;
