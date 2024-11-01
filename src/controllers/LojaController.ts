2ff51acb10b66821eaf9eb94f6e5121e7c3d4a3e
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-11-01 02:03:08 -0300

    Finalização da funçao buscarLojaMaisProxima

--- src/controllers/LojaController.ts
+++ src/controllers/LojaController.ts
@@ -1,7 +1,8 @@
-import {Request, Response} from 'express'
-import {LojaService} from '../services/lojaService'
-import {getDistancia} from '../api/ors'
+import { Request, Response } from 'express'
+import { LojaService } from '../services/lojaService'
+import { getDistancia } from '../api/ors'
 import { obterCoordenadas } from '../api/geocode';
+import { getEndereco } from '../api/viaCep';
 import { validateLojaStore, validateLojaUpdate, validateId } from '../validations/lojaValidation';
 
 
@@ -74,13 +75,28 @@ class LojaController{
             lojas.map(async (element) => {
                 const coordenada = await obterCoordenadas(element.cep);
                 const distancia = await getDistancia(cepInput, coordenada);
+
+                const {nome, logradouro, numero, cep, telefone, email} = element;
+
                 if(Number(distancia) <= 100){
-                    return { element, distancia }
+                    const {bairro, localidade, estado, regiao} = await getEndereco(element.cep);
+                    return { nome, logradouro, numero, cep, ...(bairro && {bairro}), cidade:localidade, estado, regiao, ...(telefone && {telefone}), ...(email && {email}), distancia}
                 }
+                return null;
             })
-        )).sort((lojaA, lojaB) => lojaA!.distancia - lojaB!.distancia);
+        )).filter((loja) => loja !== null)
+        .sort((lojaA, lojaB) => lojaA!.distancia - lojaB!.distancia);
         
-        return res.status(200).json(lojasProximas);
+        if(lojasProximas.length === 0){
+            return res.status(404).json({message: 'Nenhuma loja encontrada a uma distância de 100 km.'})
+        }
+
+        const lojasDistanciaFormatada = lojasProximas.map(loja => ({
+            ...loja,
+            distancia: `${Number(loja!.distancia).toFixed(2)} km`
+        }))
+
+        return res.status(200).json(lojasDistanciaFormatada);
     }
 }
 
